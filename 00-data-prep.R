c3a55fa92b17cdea3ca2bdd5974041531918e3bb
Francisca <frcastrog@uc.cl>
2024-01-22 11:47:17 +0100

    initial commit

---
+++ 00-data-prep.R
@@ -0,0 +1,7 @@
+#-------------------------------Data Preparation-------------------------------#
+#-Author: Francisca Castro ------------------------- Created: January 22, 2024-#
+#-R Version: 4.3.1 --------------------------------- Revised: January 22, 2024-#
+
+# 1) Open LAPOP Mexico datasets
+
+# 2004
